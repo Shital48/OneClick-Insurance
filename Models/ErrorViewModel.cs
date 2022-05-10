a8363fd6e04f3b6f62135162ec7f5c49ac2c4985
Shivam Bhawar <81524682+shivam-bhawar@users.noreply.github.com>
2022-05-10 10:27:06 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InsuranceSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
